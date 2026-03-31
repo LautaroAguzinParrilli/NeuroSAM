2f5f571a8fbc1d9c322af904233ff67efed41000
LautaroAguzinParrilli <ljaguzinparrilli@estudiantes.unsam.edu.ar>
2026-03-31 15:27:06 -0300

    tidy for demo

--- Training/train.py
+++ Training/train.py
@@ -17,12 +17,10 @@ if __name__ == "__main__":
 
 # -------- data --------
     df = pd.read_csv("training_data.csv") 
-    test_df = pd.read_csv("ext_test_data.csv")  # external testing 
 
     # Separar en train (90%), val (10%) 
-    train_df, val_df = train_test_split(df, test_size=0.1, random_state=42)
-
-    #val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42) #training on full database and testing on externals
+    train_df, val_df = train_test_split(df, test_size=0.8, random_state=42)
+    val_df, test_df = train_test_split(val_df, test_size=0.5, random_state=42) #training on full database and testing on externals
 
     # Extrae las listas de rutas y edades para cada set
     train_imgs = train_df["Path"].tolist()
@@ -55,20 +53,22 @@ if __name__ == "__main__":
         num_workers=8
     )
 
-# -------- model --------
+# -------- Cargo el modelo --------
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = torch.nn.DataParallel(build_vit3d()).to(device)
     print(device)
+
 # -------- training --------
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.AdamW(
+    criterion = nn.MSELoss() #funcion de costo
+    optimizer = torch.optim.AdamW( #optimizador
         model.parameters(),  
         lr=1e-4,
-        weight_decay=1e-4
+        weight_decay=1e-4 #para penalizar pesos grandes y evitar overfitting
     )
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, verbose=True)
     model.train()
-    num_epochs = 250
+    num_epochs = 250 #numero de epocas
 
     # Early stopping params
     patience = 23  # Número de épocas sin mejora para detener
@@ -88,29 +88,27 @@ if __name__ == "__main__":
     ax.legend()
 
     for epoch in range(num_epochs):
+
+        #entrenamiento
         model.train()
         train_loss = 0.0
-        for imgs, ages in train_loader:
-            assert not torch.isnan(imgs).any(), "Hay NaN en las imágenes"
-            assert not torch.isinf(imgs).any(), "Hay Inf en las imágenes"
-            assert not torch.isnan(ages).any(), "Hay NaN en las edades"
-            assert not torch.isinf(ages).any(), "Hay Inf en las edades"
-    
+        for imgs, ages in train_loader: #itera sobre los batches del dataloader
             imgs = imgs.to(device)
             ages = ages.to(device).unsqueeze(1)
             preds = model(imgs)
             loss = criterion(preds, ages)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            optimizer.zero_grad() #reseter los gradientes
+            loss.backward() #calcula los gradientes
+            optimizer.step() #actualiza los pesos del modelo en funcion del gradiente
             train_loss += loss.item() * imgs.size(0)
 
-        train_loss /= len(train_loader.dataset)
+        train_loss /= len(train_loader.dataset) #promedio del loss por muestra
 
         # Validación
         model.eval()
         val_loss = 0.0
+
         with torch.no_grad():
             for imgs, ages in val_loader:
                 imgs = imgs.to(device)
@@ -130,7 +128,8 @@ if __name__ == "__main__":
         ax.autoscale_view()
         plt.draw()
         plt.pause(0.01)
-        scheduler.step(val_loss)
+
+        scheduler.step(val_loss) #ajusta el learning rate en funcion de la val_loss
 
         print(f"Epoch {epoch+1} | Train Loss: {train_loss:.3f} | Val Loss: {val_loss:.3f}")
 
@@ -138,10 +137,10 @@ if __name__ == "__main__":
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             epochs_no_improve = 0
-            best_model_state = model.state_dict()
+            best_model_state = model.state_dict() #guardo el modelo con la mejor val_loss
         else:
             epochs_no_improve += 1
-            if epochs_no_improve >= patience:
+            if epochs_no_improve >= patience: #si no hay mejoras durante 'patience' épocas, detengo el entrenamiento 
                 print(f"Early stopping en la época {epoch+1}. Mejor val_loss: {best_val_loss:.3f}")
                 if best_model_state is not None:
                     model.load_state_dict(best_model_state)
@@ -149,13 +148,14 @@ if __name__ == "__main__":
 
     plt.ioff()
     plt.savefig("loss_plot.png")  
-    torch.save(model.state_dict(), 'model.pth')
+    torch.save(model.state_dict(), 'model.pth') #guardo el modelo entrenado
 
     # -------- Evaluación en el set de test --------
     model.eval()
     test_loss = 0.0
     all_preds = []
     all_ages = []   
+
     with torch.no_grad():
         for imgs, ages in test_loader:
             imgs = imgs.to(device)
@@ -166,6 +166,7 @@ if __name__ == "__main__":
             all_preds.append(preds.cpu())
             all_ages.append(ages.cpu())
     test_loss /= len(test_loader.dataset)
+
     # Calcular MAE
     all_preds = torch.cat(all_preds).numpy()
     all_ages = torch.cat(all_ages).numpy()
